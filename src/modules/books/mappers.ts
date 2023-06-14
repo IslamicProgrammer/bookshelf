23b8886bce0e5400197aa09d5fc40e4a257b105d
Murodjon Azamov <63091703+IslamicProgrammer@users.noreply.github.com>
2023-06-15 02:06:33 +0500

    fix: removed unnecessary files

--- src/modules/books/mappers.ts
+++
@@ -1,7 +0,0 @@
-import get from "lodash/get";
-
-import type * as Types from "./types";
-
-export const Books = (
-  data: Types.IEntity.Books.Main["data"]
-): Types.IEntity.Books.Main => data.map((item) => ({}));
